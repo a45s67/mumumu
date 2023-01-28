0e1f39369c0f0f66a7e6ff548bd658ad6df4bfe5
Fish <b45s67@gmail.com>
2023-01-29 02:09:31 +0800

    chore: Some url for render test

--- mumumu.go
+++ mumumu.go
@@ -15,13 +15,29 @@ func main() {
 	ec.listenSignal()
 
 	// If file is in current directory. This can also be a URL to an image or gif.
-	filePath := "./gif/bocchi-the-rock-bocchi-the-rock-gif.gif"
+    // filePath := "https://media.tenor.com/nIfKxqBUqQQAAAAC/shake-head-anime.gif"
+    // filePath := "https://media.tenor.com/nQbnkbBw3EUAAAAC/bocchi-bocchi-the.gif"
+    // filePath := "https://media.tenor.com/JFLRMx4dpScAAAAC/bocchi-the-rock-bocchi-the-rock-gif.gif"
+    // filePath := "https://media.tenor.com/nB9YSFNoDi8AAAAC/bocchi-the-rock-chainsaw-man.gif"
+    // filePath := "https://media.tenor.com/Kif5V8DkNgYAAAAC/bocchi-the-rock-bocchi.gif"
+    // filePath := "https://media1.tenor.com/images/77a28a3d1343fcc2482fa835091418b3/tenor.gif?itemid=27477401"
+    // filePath := "https://media.tenor.com/rSryJTbRc4YAAAAC/bocchi-the-rock-kita-ikuyo.gif"
+    // filePath := "https://media1.tenor.com/images/2ddebc5c19dda8a602591782ab19ffcb/tenor.gif?itemid=27477556"
+    // filePath := "https://media.tenor.com/526-Kgq17kMAAAAC/bocchi-the-rock-bocchi.gif"
+    // filePath := "https://i0.hdslb.com/bfs/article/b8c7c7f6a34280e5f6cf2c75b422d949a52bd199.gif"
+    filePath := "https://media1.tenor.com/images/ef715b505f47d21418b497f4c1d7fbb0/tenor.gif?itemid=27478087"
+    // filePath := "https://i0.hdslb.com/bfs/article/43ab8d5f2a35d29d5a7ef9246c7dc0c7b7495a57.gif"
+    // filePath := "https://tenor.com/zh-TW/view/bocchi-the-rock-chainsaw-man-hitori-goto-gif-27061336"
+    // filePath := "https://media.tenor.com/VyugKLEBolsAAAAC/bocchi-bocchi-the-rock.gif"
+    // filePath := "https://media.tenor.com/nQbnkbBw3EUAAAAC/bocchi-bocchi-the.gif"
+	// filePath := "./gif/bochhi.gif"
 
 	flags := aic_package.DefaultFlags()
 
-	flags.Braille = true
+	flags.Braille = false
+	flags.Threshold = 30
 	flags.Colored = true
-	// flags.CustomMap = " .-=+#@"
+	flags.CustomMap = ""
 
 	// Note: For environments where a terminal isn't available (such as web servers), you MUST
 	// specify atleast one of flags.Width, flags.Height or flags.Dimensions
@@ -34,6 +50,6 @@ func main() {
 		startTime:   time.Now(),
 	}
 
-    gr.loadGifToAscii()
-    gr.renderGif(&ec)
+	gr.loadGifToAscii()
+	gr.renderGif(&ec)
 }
